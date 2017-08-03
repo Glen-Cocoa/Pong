966268b69b209916731425127aa221b956c8e8b5
Glen Cocoa <eli.steiger@gmail.com>
2017-08-03 23:23:43 +0000

    updated copies

--- pong.js
+++ pong.js
@@ -5,6 +5,16 @@ class Vec
 			this.x = x;
 			this.y = y;
 		}
+		get len()
+		{
+			return Math.sprt(this.x * this.x + this.y * this.y);
+		}
+		set len(value)
+		{
+			const fat = value / this.len;
+			this.x *= fact;
+			this.y *= fact;
+		}
 	}
 
 class Rect
@@ -79,14 +89,48 @@ class Pong
 				requestAnimationFrame(callback);
 			}
 			callback();
-	
+			
+			this.CHAR_PIXEL= 10;
+			this.CHARS = [
+			'111101101101111',
+			'010010010010010',
+			'111001111100111',
+			'111001111001111',
+			'101101111001001',
+			'111100111001111',
+			'111100111101111',
+			'111001001001001',
+			'111101111101111',
+			'111101111001111',
+			].map(str =. {
+				const canvas = document.creatElement(canvas);
+				canvas.height = this.CHAR_PIXEL * 5;
+				canvas.width = this.CHAR_PIXEL * 3;
+				const context = canvas.getContext('2d');
+				context.fillStyle = '#fff';
+				str.split('').forEach((fill, i => {
+					if(fill === '1'){
+						context.fillRect(
+						(i % 3) * this.CHAR_PIXEL,
+						(i/3 | 0) * this.CHAR_PIXEL,
+						this.CHAR_PIXEL,
+						this.CHAR_PIXEL);
+						}
+					})
+					return canvas;
+				})
+				
+
 			this.reset();
 	}
 	collide(player, ball)
 	{
 		if(player.left < ball.right && player.right > ball.left &&
 			player.top<ball.bottom && player.bottom > ball.top) {
-			ball.vel.x = -ball.vel.x
+			const len = ball.vel.len;
+			ball.vel.y += 300 * (Math.random() - .5);
+			ball.vel.x = -ball.vel.x;
+			ball.vel.len = len * 1.05;
 			}
 		}
 	draw()
@@ -97,6 +141,8 @@ class Pong
 	
 		this.drawRect(this.ball); 
 		this.players.forEach(player => this.drawRect(player));
+
+		this.drawScore();
 	}
 
 	drawRect(rect)
@@ -106,6 +152,23 @@ class Pong
 								rect.size.x, rect.size.y);
 	}
 
+	drawScore()
+	{
+		const align = this._canvas.width/3;
+		const CHAR_W = this.CHAR_PIXEL * 4;
+		this.players.forEach((player, index) => {
+			const chars = players.score.toString().split('');
+			const offset = align * 
+							(index + 1) - 
+							(CHAR_W * chars.length /2) + 
+							this.CHAR_PIXEL / 2;
+					chars.forEach((char,pos) => {
+						this._context.drawImage(this.CHARS[char|0],
+												offset + pos * CHAR_W, 20);
+					}
+			})
+		}
+
 	reset()
 	{
 			this.ball.pos.x = this._canvas.width / 2;
@@ -114,7 +177,16 @@ class Pong
 			this.ball.vel.y = 0;
 		
 	}
-	
+
+	start()
+	{
+		if (this.ball.vel.x === 0 &&& this.ball.vel.y === 0){
+			this.ball.vel.x =300; * (Math.random() > .5 ? 1 :-1);
+			this.ball.vel.y =300; * (Math.random() *2 -1);
+			this.ball.vel.len = 200;
+		}
+	}
+
 	update(dt){
 		this.ball.pos.x += this.ball.vel.x * dt;
 		this.ball.pos.y += this.ball.vel.y * dt;
@@ -143,5 +215,37 @@ const canvas = document.getElementById('pong');
 const pong = new Pong(canvas);
 
 canvas.addEventListener('mousemove',event = {
-	pong.players[0].pos.y = event.offsetY
+	const scale = event.offsetY / event.target.getBoundingClientRect().height;
+	pong.players[0].pos.y = canvas.height * scale;
 	});
+	
+canvas.addEventListener('click',event = {
+	pong.start();
+	}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
